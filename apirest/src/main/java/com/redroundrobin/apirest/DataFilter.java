365a5d698d80f1c11d67aa287e6d1c7c1664be68
Mariano <marianosciacco@gmail.com>
2020-02-25 11:24:38 +0100

    Update DataFilter.java

--- apirest/src/main/java/com/redroundrobin/apirest/DataFilter.java
+++ apirest/src/main/java/com/redroundrobin/apirest/DataFilter.java
@@ -1,8 +1,30 @@
 package com.redroundrobin.apirest;
 
+import com.google.gson.JsonObject;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class DataFilter {
 
+    List<JsonObject> jsonData = new ArrayList<JsonObject>();
 
+    public DataFilter(){ }
+
+    public void setJsonData(List<JsonObject> jd) {
+        jsonData = jd;
+    }
+
+    public List<JsonObject> filterByTime(Duration delay) {
+
+        long time = System.currentTimeMillis();
+
+        List<JsonObject> filteredData = jsonData.stream()
+                                                .filter(x -> x.get("timestamp").getAsLong() >= time - delay.toMillis())
+                                                .collect(Collectors.toList());
+
+        return filteredData;
+    }
 
 }
