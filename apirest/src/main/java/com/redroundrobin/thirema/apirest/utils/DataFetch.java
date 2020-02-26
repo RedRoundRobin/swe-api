0ee9682a38b3d16fff5797b43d3ef461506d4458
Mariano <marianosciacco@gmail.com>
2020-02-26 16:12:57 +0100

    Update DataFetch.java

--- apirest/src/main/java/com/redroundrobin/thirema/apirest/utils/DataFetch.java
+++ apirest/src/main/java/com/redroundrobin/thirema/apirest/utils/DataFetch.java
@@ -1,8 +1,6 @@
 package com.redroundrobin.thirema.apirest.utils;
 import com.google.gson.JsonObject;
 
-import static com.redroundrobin.thirema.apirest.utils.Consumatore.fetchMessage;
-
 import java.util.List;
 
 public class DataFetch {
@@ -10,7 +8,7 @@ public class DataFetch {
     public static List<JsonObject> getForTopics(String [] topics) throws InterruptedException {
 
         Consumatore cons = new Consumatore(topics, "localhost:29092");
-        List<JsonObject> mex = fetchMessage(cons);
+        List<JsonObject> mex = cons.fetchMessage(cons);
         cons.chiudi();
         return mex;
     }
