7480b7236952ea1ed7fcf89c077e2a8e36af8695
Mariano <marianosciacco@gmail.com>
2020-02-28 17:30:39 +0100

    Update DataFetch.java

--- apirest/src/main/java/com/redroundrobin/thirema/apirest/utils/DataFetch.java
+++ apirest/src/main/java/com/redroundrobin/thirema/apirest/utils/DataFetch.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.redroundrobin.thirema.apirest.models.Device;
 
 
 import java.util.ArrayList;
@@ -21,36 +22,22 @@ public class DataFetch {
         return mex;
     }
 
-    public static List<JsonObject> testMessage() {
-        // TEST STRING GENERATOR
-        // ============================
-        String mytest = "[{\"deviceId\":1,\"timestamp\":1582818576871,\"sensors\":[{\"sensorId\":1,\"timestamp\":1582818576620,\"data\":5},{\"sensorId\":2,\"timestamp\":1582818576871,\"data\":5}]}, {\"deviceId\":2,\"timestamp\":1582818577121,\"sensors\":[{\"sensorId\":1,\"timestamp\":1582818577121,\"data\":9}]}]";
-                ;
-        List<JsonObject> mex = new ArrayList<JsonObject>();
-        JsonArray dati = new JsonParser().parseString(mytest).getAsJsonArray();
-        for (JsonElement elemento : dati) {
-            JsonObject datiDispositivo = elemento.getAsJsonObject();
-            mex.add(datiDispositivo);
-        }
-        return mex;
-        // Usare "mex" come List<JsonObject> di output da un possibile consumatore.
-        // ============================
-    }
-
-/*
     public static List<JsonObject> getDevices(){
         List<JsonObject> test = testMessage();
         List<JsonObject> devices = new ArrayList<JsonObject>();
         devices = test.stream()
-            .filter(jsonObject -> jsonObject.get("deviceId").getAsString() != "").collect(Collectors.toList());
+                .filter(jsonObject -> jsonObject.get("deviceId").getAsString() != "").collect(Collectors.toList());
         return devices;
     }
-    public static List<JsonObject> getDevice(String deviceId){
-        List<JsonObject> test = testMessage();
-        List<JsonObject> device = new ArrayList<JsonObject>();
-        device = test.stream()
-                .filter(jsonObject -> jsonObject.get("deviceId").getAsString().equals(deviceId)).collect(Collectors.toList());
-        return device;
+    public static Device getDevice(String deviceId){
+        List<JsonObject> mex = testMessage();
+        List<JsonObject> deviceFilter = new ArrayList<JsonObject>();
+        Optional<>
+        deviceFilter = mex.stream()
+                            .filter(jo -> jo.get("deviceId").getAsString().equals(deviceId)).findFirst());
+
+
+        return new Device(deviceFilter.get(0).get("deviceId").getAsInt());
     }
     public static List<JsonObject> getSensor(String deviceId, String sensorId){
         List<JsonObject> test = testMessage();
@@ -62,10 +49,24 @@ public class DataFetch {
         sensor = sensor.get("sensors").getAsJsonArray();
 
         sensor = sensor.stream() .filter(jsonObject -> {
-                jsonObject.get("sensorId").getAsString().equals(sensorId)})
+            jsonObject.get("sensorId").getAsString().equals(sensorId)})
                 .collect(Collectors.toList());
         return sensor;
     }
 
- */
+    public static List<JsonObject> testMessage() {
+        // TEST STRING GENERATOR
+        // ============================
+        String mytest = "[{\"deviceId\":1,\"timestamp\":1582818576871,\"sensors\":[{\"sensorId\":1,\"timestamp\":1582818576620,\"data\":5},{\"sensorId\":2,\"timestamp\":1582818576871,\"data\":5}]}, {\"deviceId\":2,\"timestamp\":1582818577121,\"sensors\":[{\"sensorId\":1,\"timestamp\":1582818577121,\"data\":9}]}]";
+        ;
+        List<JsonObject> mex = new ArrayList<JsonObject>();
+        JsonArray dati = new JsonParser().parseString(mytest).getAsJsonArray();
+        for (JsonElement elemento : dati) {
+            JsonObject datiDispositivo = elemento.getAsJsonObject();
+            mex.add(datiDispositivo);
+        }
+        return mex;
+        // Usare "mex" come List<JsonObject> di output da un possibile consumatore.
+        // ============================
+    }
 }
