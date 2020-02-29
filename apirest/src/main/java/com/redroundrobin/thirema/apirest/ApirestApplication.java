d1f1c7fe7ba1a1836c0a53cc117ee00a9af8cd00
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2020-02-29 13:26:00 +0100

    Revert "Refactoring"

--- apirest/src/main/java/com/redroundrobin/thirema/apirest/ApirestApplication.java
+++ apirest/src/main/java/com/redroundrobin/thirema/apirest/ApirestApplication.java
@@ -1,5 +1,6 @@
 package com.redroundrobin.thirema.apirest;
 
+import com.redroundrobin.thirema.apirest.utils.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- apirest/src/main/java/com/redroundrobin/thirema/apirest/models/DeviceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.redroundrobin.thirema.apirest.models;
-
-public class DeviceNotFoundException extends Exception {
-    public DeviceNotFoundException(String message) {
-        super(message);
-    }
-}
