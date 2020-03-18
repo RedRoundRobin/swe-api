66ee85e7354ec745d273c81b828bb73910ae7a39
Lorenzo Dei Negri <lorenzodeinegri@gmail.com>
2020-03-18 13:52:18 +0100

    Aggiunto SonarCloud

--- apirest/src/test/java/com/redroundrobin/thirema/apirest/ApirestApplicationTests.java
+++ apirest/src/test/java/com/redroundrobin/thirema/apirest/ApirestApplicationTests.java
@@ -1,13 +1,13 @@
 package com.redroundrobin.thirema.apirest;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApirestApplicationTests {
+public class ApirestApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
---
+++ apirest/src/test/java/com/redroundrobin/thirema/apirest/models/TopicTest.java
@@ -0,0 +1,13 @@
+package com.redroundrobin.thirema.apirest.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TopicTest {
+
+    @Test
+    public void setMessage() {
+        assertTrue(true);
+    }
+}
