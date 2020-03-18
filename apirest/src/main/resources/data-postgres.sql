e093d8186e5b550e0f03313513e28f9e593d2fdb
Fritz <nicolo.frison.1@studenti.unipd.it>
2020-03-18 15:57:12 +0100

    Rimossi sql da apirest

--- apirest/src/main/resources/data-postgres.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO gateways(gateway_id, name) VALUES(1, 'Gateway US');
-INSERT INTO gateways(gateway_id, name) VALUES(2, 'Gateway SG');
-INSERT INTO devices(device_id, name, frequency, gateway_id) VALUES(1, 'Device n1', 1, 1);
-INSERT INTO devices(device_id, name, frequency, gateway_id) VALUES(2, 'Device n2', 2, 1);
-INSERT INTO devices(device_id, name, frequency, gateway_id) VALUES(3, 'Device n1', 1, 2);
-INSERT INTO sensors(sensor_id, type, device_sensor_id, device_id) VALUES(1, 'Sensor n1', 1, 1);
-INSERT INTO sensors(sensor_id, type, device_sensor_id, device_id) VALUES(2, 'Sensor n2', 2, 1);
-INSERT INTO sensors(sensor_id, type, device_sensor_id, device_id) VALUES(3, 'Sensor n1', 1, 2);
