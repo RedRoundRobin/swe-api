4bb74390455f67f29cfa0c5674aa4b59e01a80f5
Fritz <nicolo.frison.1@studenti.unipd.it>
2020-03-21 16:14:00 +0100

    Spostato metodi autenticazione in controller a parte

--- apirest/src/main/java/com/redroundrobin/thirema/apirest/controller/PostgreController.java
+++ apirest/src/main/java/com/redroundrobin/thirema/apirest/controller/PostgreController.java
@@ -1,8 +1,5 @@
 package com.redroundrobin.thirema.apirest.controller;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.redroundrobin.thirema.apirest.models.AuthenticationRequest;
 import com.redroundrobin.thirema.apirest.models.postgres.Device;
 import com.redroundrobin.thirema.apirest.models.postgres.Gateway;
 import com.redroundrobin.thirema.apirest.models.postgres.Sensor;
@@ -11,22 +8,10 @@ import com.redroundrobin.thirema.apirest.service.postgres.SensorService;
 import com.redroundrobin.thirema.apirest.service.postgres.DeviceService;
 import com.redroundrobin.thirema.apirest.service.postgres.GatewayService;
 import com.redroundrobin.thirema.apirest.service.postgres.UserService;
-import com.redroundrobin.thirema.apirest.utils.JwtUtil;
-import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
-
-import org.springframework.http.HttpEntity;
 
 import java.util.stream.Collectors;
 
@@ -146,140 +131,8 @@ public class PostgreController {
     System.out.println(payload);
   }
 
-  //funzione di test per ottenere l'username dal token
-  @GetMapping(value = "/credentials")
-  public String getCredentials(@RequestHeader("Authorization") String authorization) {
-    String token = authorization.substring(7);
-
-    return jwtTokenUtil.extractUsername(token);
-  }
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  @Autowired
-  private AuthenticationManager authenticationManager;
-
-  @Autowired
-  private JwtUtil jwtTokenUtil;
-
-  @Value("${telegram.url}")
-  private String telegramUrl;
-
-  @RequestMapping(value = "/auth", method = RequestMethod.POST)
-  public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
-    String email = authenticationRequest.getUsername();
-    String password = authenticationRequest.getPassword();
-
-    if( email == null || password == null ) {
-      return ResponseEntity.status(400).build();  // Bad Request
-    }
-
-    try {
-      authenticationManager.authenticate(
-          new UsernamePasswordAuthenticationToken(email, password)
-      );
-    } catch (BadCredentialsException bce) {
-      return ResponseEntity.status(401).build();  // Unauthenticated
-    } catch (DisabledException de) {
-      return ResponseEntity.status(403).build();  // Unauthorized
-    }
-
-    HashMap<String,Object> response = new HashMap<>();
-    final User user = userService.findByEmail(email);
-
-
-    final UserDetails userDetails = userService
-        .loadUserByUsername(authenticationRequest.getUsername());
-    String token;
-
-    if(user.getTFA()){
-
-      Random rnd = new Random();
-      int sixDigitsCode = 100000 + rnd.nextInt(900000);
-
-      Map<String, Object> map = new HashMap<>();
-      map.put("auth_code", sixDigitsCode); //codice fittizio
-      map.put("chat_id", user.getTelegramChat());
-      HttpEntity<Map<String, Object>> entity = new HttpEntity<>(map);
-
-      RestTemplate restTemplate = new RestTemplate();
-      ResponseEntity<String> telegramResponse = restTemplate.postForEntity(telegramUrl, entity, String.class);
-
-      response.put("tfa", true);
-
-      token = jwtTokenUtil.generateTfaToken(userDetails, sixDigitsCode);
-    } else {
-      response.put("user", user);
-
-      token = jwtTokenUtil.generateToken(userDetails);
-    }
-
-    response.put("token", token);
-
-    return ResponseEntity.ok(response);
-  }
-
-  @PostMapping(value = "/auth/tfa")
-  public ResponseEntity<?> checkTFA(@RequestBody String rawData, @RequestHeader("Authorization") String authorization) {
-    JsonObject data = JsonParser.parseString(rawData).getAsJsonObject();
-
-    if( !data.has("auth_code") || data.get("auth_code").getAsString().equals("") ) {
-      return ResponseEntity.status(400).build();
-    }
-
-    int authCode = data.get("auth_code").getAsInt();
-    String tfaToken = authorization.substring(7);
-    int tokenAuthCode = jwtTokenUtil.extractAuthCode(tfaToken);
-
-    if( authCode == tokenAuthCode ) {
-      User user = userService.findByEmail(jwtTokenUtil.extractUsername(tfaToken));
-
-      final UserDetails userDetails = userService
-          .loadUserByUsername(user.getEmail());
-      final String token = jwtTokenUtil.generateToken(userDetails);
-
-      HashMap<String, Object> response = new HashMap<>();
-      response.put("token", token);
-      response.put("user", user);
-
-      return ResponseEntity.ok(response);
-    } else {
-      return ResponseEntity.status(401).build();
-    }
-  }
-
-  @PostMapping(value = "/check")
-  public ResponseEntity<?> checkToken(@RequestHeader("Authorization") String authorization) {
-    if( authorization != null ) {
-      String token = authorization.substring(7);
-      HashMap<String, Object> response = new HashMap<>();
-
-      try {
-        Date expirationTime = jwtTokenUtil.extractExpiration(token);
-
-        if (expirationTime.after(new Date())) {
-          response.put("valid", true);
-        } else {
-          response.put("valid", false);
-        }
-      } catch (ExpiredJwtException eje) {
-        response.put("valid", false);
-      }
-      return ResponseEntity.ok(response);
-    } else {
-      return ResponseEntity.status(400).build();
-    }
-  }
-
-  //funzione di controllo username Telegram e salvataggio chatID
-  @GetMapping(value = {"/login/{username:.+}/{chatId:.+}"})
-  public int checkUser(@PathVariable("username") String username, @PathVariable("chatId") String chatId) {
-    if (userService.findByTelegramName(username) == null)
-      return 0;
-    if (userService.findByTelegramNameAndTelegramChat(username, chatId) == null)
-      return 1;
-    return 2;
-  }
 }
 
 /*
