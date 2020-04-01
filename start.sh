3750d7c5835b819debe3d4d08f1aacb684f95f14
Lorenzo Dei Negri <lorenzodeinegri@gmail.com>
2020-04-01 21:37:07 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,12 +3,12 @@ cd apirest
 
 mvn clean package
 
-mv ./targer/apirest-*.jar ../
+mv ./target/apirest-*.jar ../../apirest.jar
 
 cd ../..
 
 rm -rf temp 
 
-java -jar /usr/src/api/apirest-*.jar --server.port=9999
+java -jar /usr/src/api/apirest.jar --server.port=9999
 
 echo "API started..."
