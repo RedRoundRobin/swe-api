42275fe21e8761a1cc6f03134a4d1ee07b9c3750
Lorenzo Dei Negri <lorenzodeinegri@gmail.com>
2020-04-01 23:14:32 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,9 @@ mv ./target/apirest-*.jar ../../apirest.jar
 cd ../..
 rm -rf tmp 
 
-java -jar /usr/src/api/apirest.jar --server.port=9999 --spring.postgres.url=jdbc:postgresql://core.host.redroundrobin.site:6543/postgre --spring.timescale.url=jdbc:postgresql://core.host.redroundrobin.site:3456/timescale
+java -jar /usr/src/api/apirest.jar \
+--server.port=9999 --spring.postgres.url=jdbc:postgresql://core.host.redroundrobin.site:6543/postgre \
+--spring.timescale.url=jdbc:postgresql://core.host.redroundrobin.site:3456/timescale \
+--telegram.url=http://core.host.redroundrobin.site:3000/
 
 echo "API started..."
