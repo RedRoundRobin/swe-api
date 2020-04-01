85f238be03ab5bc84599eac99b8a5d029aab915e
Fritz <nicolo.frison.1@studenti.unipd.it>
2020-04-01 13:12:21 +0200

    Sistemati models timescale

--- apirest/src/main/java/com/redroundrobin/thirema/apirest/models/timescale/Alerts.java
+++ apirest/src/main/java/com/redroundrobin/thirema/apirest/models/timescale/Alerts.java
@@ -15,29 +15,54 @@ public class Alerts {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Timestamp time;
 
-  @Column(name = "sensor_id", nullable = false)
-  private int sensorId;
+  @Column(name = "gateway_id", nullable = false)
+  private int gatewayId;
 
   @Column(name = "device_id", nullable = false)
   private int deviceId;
 
-  private double value;
+  @Column(name = "sensor_id", nullable = false)
+  private int sensorId;
 
+  private double value;
 
   public Timestamp getTime() {
     return time;
   }
 
-  public int getSensorId() {
-    return sensorId;
+  public void setTime(Timestamp time) {
+    this.time = time;
+  }
+
+  public int getGatewayId() {
+    return gatewayId;
+  }
+
+  public void setGatewayId(int gatewayId) {
+    this.gatewayId = gatewayId;
   }
 
   public int getDeviceId() {
     return deviceId;
   }
 
+  public void setDeviceId(int deviceId) {
+    this.deviceId = deviceId;
+  }
+
+  public int getSensorId() {
+    return sensorId;
+  }
+
+  public void setSensorId(int sensorId) {
+    this.sensorId = sensorId;
+  }
+
   public double getValue() {
     return value;
   }
 
+  public void setValue(double value) {
+    this.value = value;
+  }
 }
--- apirest/src/main/java/com/redroundrobin/thirema/apirest/models/timescale/Logs.java
+++ apirest/src/main/java/com/redroundrobin/thirema/apirest/models/timescale/Logs.java
@@ -15,15 +15,42 @@ public class Logs {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Timestamp time;
 
-  @Column(name = "log_id", nullable = false)
-  private int logId;
+  @Column(name = "user_id", nullable = false)
+  private int userId;
 
+  private String operation;
+
+  private String data;
 
   public Timestamp getTime() {
     return time;
   }
 
-  public int getLogId() {
-    return logId;
+  public void setTime(Timestamp time) {
+    this.time = time;
+  }
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public void setUserId(int userId) {
+    this.userId = userId;
+  }
+
+  public String getOperation() {
+    return operation;
+  }
+
+  public void setOperation(String operation) {
+    this.operation = operation;
+  }
+
+  public String getData() {
+    return data;
+  }
+
+  public void setData(String data) {
+    this.data = data;
   }
 }
