e49faf46547322595d51bc61a6f3b9ebc8609363
Fritz <nicolo.frison.1@studenti.unipd.it>
2020-04-02 13:29:16 +0200

    Fix sensorRepository

--- apirest/src/main/java/com/redroundrobin/thirema/apirest/repository/timescale/SensorRepository.java
+++ apirest/src/main/java/com/redroundrobin/thirema/apirest/repository/timescale/SensorRepository.java
@@ -1,7 +1,6 @@
 package com.redroundrobin.thirema.apirest.repository.timescale;
 
 import java.sql.Timestamp;
-import java.util.List;
 import com.redroundrobin.thirema.apirest.models.timescale.Sensor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -13,8 +12,8 @@ public interface SensorRepository extends CrudRepository<Sensor, Timestamp> {
   Iterable<Sensor> findAllByGatewayIdAndDeviceIdAndSensorIdOrderByTimeDesc(int gatewayId, int deviceId,
                                                             int sensorId);
 
-  @Query(value = "SELECT * FROM Alert WHERE gatewayId = :gatewayId AND deviceId = :deviceId "
-      + "AND sensorId = :sensorId ORDER BY time DESC LIMIT :resultsNumber ORDER BY time DESC",
+  @Query(value = "SELECT * FROM sensors WHERE gateway_id = :gatewayId AND device_id = :deviceId "
+      + "AND sensor_id = :sensorId ORDER BY time DESC LIMIT :resultsNumber",
       nativeQuery = true)
   Iterable<Sensor> findTopNByGatewayIdAndDeviceIdAndSensorId(int resultsNumber, int gatewayId,
                                                               int deviceId, int sensorId);
