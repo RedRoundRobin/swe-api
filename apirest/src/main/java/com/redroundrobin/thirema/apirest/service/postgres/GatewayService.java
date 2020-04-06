342cf8e753fe399b88745123579814377abf21e5
Fritz <nicolo.frison.1@studenti.unipd.it>
2020-04-06 08:42:23 +0200

    Sostituiti attributi Service di GatewayService in Repository

--- apirest/src/main/java/com/redroundrobin/thirema/apirest/service/postgres/GatewayService.java
+++ apirest/src/main/java/com/redroundrobin/thirema/apirest/service/postgres/GatewayService.java
@@ -2,6 +2,7 @@ package com.redroundrobin.thirema.apirest.service.postgres;
 
 import com.redroundrobin.thirema.apirest.models.postgres.Device;
 import com.redroundrobin.thirema.apirest.models.postgres.Gateway;
+import com.redroundrobin.thirema.apirest.repository.postgres.DeviceRepository;
 import com.redroundrobin.thirema.apirest.repository.postgres.GatewayRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,35 +11,35 @@ import org.springframework.stereotype.Service;
 @Service
 public class GatewayService {
 
-  private GatewayRepository repo;
+  private GatewayRepository gatewayRepo;
 
-  private DeviceService deviceService;
+  private DeviceRepository deviceRepo;
 
   @Autowired
   public GatewayService(GatewayRepository gatewayRepository) {
-    this.repo = gatewayRepository;
+    this.gatewayRepo = gatewayRepository;
   }
 
   public List<Gateway> findAll() {
-    return (List<Gateway>) repo.findAll();
+    return (List<Gateway>) gatewayRepo.findAll();
   }
 
   public Gateway findByDeviceId(int deviceId) {
-    Device device = deviceService.findById(deviceId);
+    Device device = deviceRepo.findById(deviceId).orElse(null);
     if (device != null) {
-      return repo.findByDevices(device);
+      return gatewayRepo.findByDevices(device);
     } else {
       return null;
     }
   }
 
   public Gateway findById(int id) {
-    return repo.findById(id).orElse(null);
+    return gatewayRepo.findById(id).orElse(null);
   }
 
   @Autowired
-  public void setDeviceService(DeviceService deviceService) {
-    this.deviceService = deviceService;
+  public void setDeviceRepository(DeviceRepository deviceRepository) {
+    this.deviceRepo = deviceRepository;
   }
 
 }
