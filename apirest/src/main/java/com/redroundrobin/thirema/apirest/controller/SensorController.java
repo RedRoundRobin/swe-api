46f9a96f2364498e1d973deea53e30e2d868b607
Fritz <nicolo.frison.1@studenti.unipd.it>
2020-04-11 22:25:55 +0200

    Aggiunti logs di debug a SensorController

--- apirest/src/main/java/com/redroundrobin/thirema/apirest/controller/SensorController.java
+++ apirest/src/main/java/com/redroundrobin/thirema/apirest/controller/SensorController.java
@@ -8,6 +8,9 @@ import com.redroundrobin.thirema.apirest.service.timescale.LogService;
 import com.redroundrobin.thirema.apirest.utils.JwtUtil;
 import java.util.Collections;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = {"/sensors"})
 public class SensorController extends CoreController {
+
+  protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
   private final SensorService sensorService;
 
