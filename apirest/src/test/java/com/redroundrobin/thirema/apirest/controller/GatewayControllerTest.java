d80a0cdff3e8e598b021d68b7c648ac60678eefb
Fouad <fouadmouad5@gmail.com>
2020-05-09 22:54:06 +0200

    Coverage GatewayController 100%

--- apirest/src/test/java/com/redroundrobin/thirema/apirest/controller/GatewayControllerTest.java
+++ apirest/src/test/java/com/redroundrobin/thirema/apirest/controller/GatewayControllerTest.java
@@ -7,6 +7,9 @@ import com.redroundrobin.thirema.apirest.service.postgres.SensorService;
 import com.redroundrobin.thirema.apirest.service.postgres.UserService;
 import com.redroundrobin.thirema.apirest.service.timescale.LogService;
 import com.redroundrobin.thirema.apirest.utils.JwtUtil;
+import com.redroundrobin.thirema.apirest.utils.exception.ElementNotFoundException;
+import com.redroundrobin.thirema.apirest.utils.exception.InvalidFieldsValuesException;
+import com.redroundrobin.thirema.apirest.utils.exception.MissingFieldsException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,16 +17,25 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
@@ -49,6 +61,8 @@ public class GatewayControllerTest {
   @MockBean
   private SensorService sensorService;
 
+  MockHttpServletRequest httpRequest;
+
   private final String userTokenWithBearer = "Bearer userToken";
   private final String adminTokenWithBearer = "Bearer adminToken";
   private final String userToken = "userToken";
@@ -72,16 +86,19 @@ public class GatewayControllerTest {
   private Sensor sensor4;
 
   private List<Gateway> allGateways;
+  private List<Entity> allEntities;
 
   @Before
   public void setUp() {
+
+    httpRequest = new MockHttpServletRequest();
+    httpRequest.setRemoteAddr("localhost");
+
     gatewayController = new GatewayController(gatewayService, deviceService, sensorService, jwtUtil, logService, userService);
 
     // ----------------------------------------- Set Users --------------------------------------
     admin = new User(1, "admin", "admin", "admin", "pass", User.Role.ADMIN);
     user = new User(2, "user", "user", "user", "user", User.Role.USER);
-
-    user.setEntity(new Entity(1, "name", "loc"));
 
     // ----------------------------------------- Set Devices --------------------------------------
     device1 = new Device(1, "dev1", 1, 1);
@@ -112,6 +129,14 @@ public class GatewayControllerTest {
     allSensors.add(sensor2);
     allSensors.add(sensor3);
     allSensors.add(sensor4);
+
+    user.setEntity(new Entity(1, "name", "loc"));
+    allEntities = new ArrayList<>();
+    allEntities.add(user.getEntity());
+
+    Set<Sensor> entity1Sensors = new HashSet<>();
+    entity1Sensors.add(sensor1);
+    allEntities.get(0).setSensors(entity1Sensors);
 
     // ---------------------------------- Set Gateways to Devices -------------------------------
     device1.setGateway(gateway1);
@@ -146,6 +171,23 @@ public class GatewayControllerTest {
       } else {
         return null;
       }
+    });
+    when(gatewayService.findByDeviceIdAndEntityId(anyInt(), anyInt())).thenAnswer(i -> {
+      Entity entity = allEntities.stream().filter(e -> (i.getArgument(1)).equals(e.getId())).findFirst().orElse(null);
+      if(entity == null) {
+        return null;
+      }
+      Device device = allDevices.stream().filter(d -> (i.getArgument(0)).equals(d.getId())).findFirst().orElse(null);
+      if(device == null) {
+        return null;
+      }
+      //se all'entità passata alla funzione è abilitato almeno un sensore del device fornito, mostro il suo gateway;
+      //altrimenti restituisco null
+          if(entity.getSensors().stream().filter(
+              s -> s.getDevice().getId() == device.getId()).findFirst().orElse(null) != null) {
+            return device.getGateway();
+          }
+      return null;
     });
 
     when(deviceService.findAllByGatewayId(anyInt())).thenAnswer(i -> {
@@ -232,7 +274,7 @@ public class GatewayControllerTest {
     ResponseEntity<List<Gateway>> response = gatewayController.getGateways(userTokenWithBearer, 1);
 
     assertEquals(HttpStatus.OK, response.getStatusCode());
-    assertTrue(response.getBody().isEmpty());
+    assertEquals(gateway1, (response.getBody().get(0)));
   }
 
   @Test
@@ -322,4 +364,164 @@ public class GatewayControllerTest {
 
     assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
   }
+
+  @Test
+  public void addGatewayByAdminSuccesful()
+      throws MissingFieldsException, InvalidFieldsValuesException {
+    Map<String, Object> newGatewayFields = new HashMap<>();
+    newGatewayFields.put("name", "newGateway");
+
+    when(gatewayService.addGateway(any(Map.class))).thenAnswer(i -> {
+      Gateway gateway = new Gateway((String)((HashMap<String, Object>)i.getArgument(0)).get("name"));
+      allGateways.add(gateway);
+      return gateway;
+    });
+
+    ResponseEntity<Gateway> response = gatewayController.addGateway(
+        adminTokenWithBearer, newGatewayFields, httpRequest);
+
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    assertEquals(allGateways.get(allGateways.size()-1), response.getBody());
+  }
+
+  @Test
+  public void addGatewayByAdminMissingFieldsException()
+      throws MissingFieldsException, InvalidFieldsValuesException {
+    Map<String, Object> newGatewayFields = new HashMap<>();
+
+    when(gatewayService.addGateway(any(Map.class))).thenThrow(new MissingFieldsException(""));
+
+    ResponseEntity<Gateway> response = gatewayController.addGateway(
+        adminTokenWithBearer, newGatewayFields, httpRequest);
+
+    assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    assertTrue(response.getBody() == null);
+  }
+
+  @Test
+  public void addGatewayByUserError403() {
+    Map<String, Object> newGatewayFields = new HashMap<>();
+    newGatewayFields.put("name", "newGateway");
+
+    ResponseEntity<Gateway> response = gatewayController.addGateway(
+        userTokenWithBearer, newGatewayFields, httpRequest);
+
+    assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
+  }
+
+  @Test
+  public void editGateway1ByAdminSuccesful()
+      throws MissingFieldsException, InvalidFieldsValuesException, ElementNotFoundException {
+    Map<String, Object> editGatewayFields = new HashMap<>();
+    editGatewayFields.put("name", "gatewayTest2");
+
+    gateway1.setName((String)editGatewayFields.get("name"));
+    when(gatewayService.editGateway(anyInt(), any(Map.class))).thenReturn(gateway1);
+
+    ResponseEntity<Object> response = gatewayController.editGateway(adminTokenWithBearer,
+         gateway1.getId(), editGatewayFields, httpRequest);
+
+    assertEquals(gateway1.getName(), (String)editGatewayFields.get("name"));
+    assertEquals(gateway1, response.getBody());
+  }
+
+  @Test
+  public void editGateway1ByAdminSuccesfulMissingFieldsException()
+      throws MissingFieldsException, InvalidFieldsValuesException, ElementNotFoundException {
+    Map<String, Object> editGatewayFields = new HashMap<>();
+    editGatewayFields.put("notExistingField", "gatewayTest2");
+
+    when(gatewayService.editGateway(anyInt(), any(Map.class))).thenThrow(new MissingFieldsException(""));
+
+    ResponseEntity<Object> response = gatewayController.editGateway(adminTokenWithBearer,
+        gateway1.getId(), editGatewayFields, httpRequest);
+
+    assertEquals(new ResponseEntity(HttpStatus.BAD_REQUEST), response);
+  }
+
+  @Test
+  public void editGateway1ByUser403Error()
+      throws MissingFieldsException, InvalidFieldsValuesException, ElementNotFoundException {
+    Map<String, Object> editGatewayFields = new HashMap<>();
+    editGatewayFields.put("name", "gatewayTest2");
+
+    ResponseEntity<Object> response = gatewayController.editGateway(userTokenWithBearer,
+        gateway1.getId(), editGatewayFields, httpRequest);
+
+    assertEquals(new ResponseEntity(HttpStatus.FORBIDDEN), response);
+  }
+
+  @Test
+  public void sendConfigToGateway1ByAdminSuccesful()
+      throws MissingFieldsException, InvalidFieldsValuesException,
+      ElementNotFoundException, JsonProcessingException {
+    Map<String, Object> sendConfig = new HashMap<>();
+    sendConfig.put("reconfig", true);
+
+    when(gatewayService.sendGatewayConfigToKafka(anyInt())).thenReturn("sentConfigToGateway1");
+
+    ResponseEntity<Object> response = gatewayController.editGateway(adminTokenWithBearer,
+        gateway1.getId(), sendConfig, httpRequest);
+
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    assertEquals("sentConfigToGateway1", (String)response.getBody());
+  }
+
+  @Test
+  public void deleteGateway1ByAdmin()
+      throws ElementNotFoundException {
+    assertTrue(allGateways.remove(gateway1));
+
+    when(gatewayService.deleteGateway(eq(gateway1.getId())))
+        .thenReturn(true);
+
+    ResponseEntity response = gatewayController.deleteGateway(adminTokenWithBearer,
+        gateway1.getId(), httpRequest);
+
+    assertEquals(new ResponseEntity(HttpStatus.OK), response);
+
+  }
+
+  @Test
+  public void deleteGateway1ByAdminConflictError409()
+      throws ElementNotFoundException {
+
+    assertTrue(allGateways.remove(gateway1));
+
+    when(gatewayService.deleteGateway(eq(device1.getId())))
+        .thenReturn(false); //errore a db: device selezionato dall'admin cè ma non viene cancellato!
+
+    ResponseEntity response = gatewayController.deleteGateway(adminTokenWithBearer,
+        device1.getId(), httpRequest);
+
+    assertEquals(new ResponseEntity(HttpStatus.CONFLICT), response);
+  }
+
+  @Test
+  public void deleteNotExistingGatewayByAdminElementNotFoundException()
+      throws  ElementNotFoundException {
+
+    Gateway gatewayNotPresentInMock = new Gateway(gateway1.getId(), "gateway8");
+
+    assertFalse(allGateways.remove(gatewayNotPresentInMock));
+
+    when(gatewayService.deleteGateway(eq(gatewayNotPresentInMock.getId())))
+        .thenThrow(new ElementNotFoundException(""));
+
+    ResponseEntity response = gatewayController.deleteGateway(adminTokenWithBearer,
+        gatewayNotPresentInMock.getId(), httpRequest);
+
+    assertEquals(new ResponseEntity<>(HttpStatus.BAD_REQUEST), response);
+  }
+
+  @Test
+  public void deleteGateway1ByUserError403Forbidden()
+      throws  ElementNotFoundException {
+
+    ResponseEntity response = gatewayController.deleteGateway(userTokenWithBearer,
+        gateway1.getId(), httpRequest);
+
+    assertEquals(new ResponseEntity(HttpStatus.FORBIDDEN), response);
+  }
+
 }
